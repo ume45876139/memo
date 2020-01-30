756a4cc96695f3848f2256f5310241156e4f8ef6
ume45876139 <umefumi943827@gmail.com>
2020-01-30 21:46:45 +0900

    Add existing file

--- delete.php
+++ delete.php
@@ -3,8 +3,17 @@
 <html lang="ja">
 
 <main>
-<h2></h2>
-
-
+<h2>Practice</h2>
+<?php 
+if(is_set($_REQUEST['id']) && is_numeric($_REQUEST['id'])){
+    $id = $_REQUEST['id'];
+    $statement = $db -> prepare('DELETE FROM memos WHERE id=?');
+    $statement->execute(array('$id'));
+}
+?>
+<pre>
+    <p>メモを削除しました</p>
+    <p><a href="index.php" >戻る</a></p>
+</pre>
 </main>
 </html>
